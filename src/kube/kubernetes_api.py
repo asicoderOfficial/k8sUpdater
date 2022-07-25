2d8c7da1f82ad19d5e9ab6e0bfc5ab6abc539b65
asicoderOfficial <asicoderofficial@gmail.com>
2022-07-25 12:20:53 +0200

    Obtain the bearer token, update a deployment image tag and restart a deployment

--- src/kube/kubernetes_api.py
+++ src/kube/kubernetes_api.py
@@ -1,34 +1,39 @@
 from kubernetes import client, config
 import base64
+from datetime import datetime
 
 
 def get_bearer_token(api_instance:client.CoreV1Api, name:str, namespace:str) -> str:
-    # get the service account resource
+    """ Obtain the bearer token to authenticate for using the kubernetes API.
+
+    Args:
+        api_instance (client.CoreV1Api): The object with which we can interact with kubernetes api.
+        name (str): Name of the account.
+        namespace (str): Namespace of the account.
+
+    Returns:
+        str: The bearer token, base64 decoded.
+    """    
+    #Get the service account resource
     sa_resource = api_instance.read_namespaced_service_account(name=name, namespace=namespace)
-    # get the token associated with the service account
+    #Get the token associated with the service account
     token_resource_name = [s for s in sa_resource.secrets if 'token' in s.name][0].name
-    # get the secret resource associated with the service account
+    #Get the secret resource associated with the service account
     secret = api_instance.read_namespaced_secret(
         name=token_resource_name, namespace=namespace)
-    # get the token data out of the secret
+    #Get the token data out of the secret
     btoken = secret.data['token']
-    # the token data is base64 encoded, so we decode it
+    #The token data is base64 encoded, so we decode it
     token = base64.b64decode(btoken).decode()
 
     return token
 
 
 #TODO: Add logging of api_response and return it in a human understandable way.
-def update_docker_image(img_name:str, deployment_name:str, deployment_namespace:str, apiserver_url:str, tag:str='latest') -> None:
+def update_container_image(img_name:str, deployment_name:str, deployment_namespace:str, apiserver_url:str, tag:str='latest') -> None:
     """ Determines wether the image should be updated or not.
 
-    Args:
-        img_name (str): Name of the image (without tag nor version).
-        deployment_name (str): Deployment name.
-        deployment_namespace (str): Deployment namespace.
-        apiserver_url (str): URL with host_ip and host_port needed for connecting to the API server.
-        tag (str, optional): Tag of the image to update to. Defaults to 'latest'.
-    
+    Args:The object with which we can interact with kubernetes api.
     Returns:
         None
     """    
@@ -41,7 +46,46 @@ def update_docker_image(img_name:str, deployment_name:str, deployment_namespace:
     with client.ApiClient(configuration) as api_client:
         # Create an instance of the API class
         api_instance = client.AppsV1Api(api_client)
-    client.configuration.debug = True
-    body = {'spec':{'template':{'spec':{'containers':[{'name':img_name,'image':f'{img_name}:{tag}'}]}}}}
-    deployment_name = 'nginx-deployment'
-    api_instance.patch_namespaced_deployment(deployment_name, deployment_namespace, body, pretty=True)
+    if tag == 'latest':
+        restart_deployment(api_instance, deployment_name, deployment_namespace)
+    else:
+        body = {
+            'spec':{
+                'template':{
+                    'spec':{
+                        'containers':[
+                            {'name':img_name,
+                            'image':f'{img_name}:{tag}'}
+                            ]
+                        }
+                    }
+                }
+            }
+        api_instance.patch_namespaced_deployment(deployment_name, deployment_namespace, body, pretty=True)
+
+
+def restart_deployment(api_instance:client.CoreV1Api, deployment:str, namespace:str) -> None:
+    """ Restarts the given deployment. It is needed when the tag to update to, is the same (latest).
+
+    Args:
+        api_instance (client.CoreV1Api): The object with which we can interact with kubernetes api.
+        deployment (str): Deployment name
+        namespace (str): Deployment namespace
+    
+    Returns:
+        None
+    """
+    now = datetime.utcnow()
+    now = str(now.isoformat("T") + "Z")
+    body = {
+        'spec': {
+            'template':{
+                'metadata': {
+                    'annotations': {
+                        'kubectl.kubernetes.io/restartedAt': now
+                    }
+                }
+            }
+        }
+    }
+    api_instance.patch_namespaced_deployment(deployment, namespace, body, pretty='true')
