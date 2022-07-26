0fd8346c5ad46e1ddec51a461ccd747c70918217
asicoderOfficial <asicoderofficial@gmail.com>
2022-07-26 11:06:20 +0200

    gitlab api valueerror exception taken into account

--- src/gitlab/api.py
+++ src/gitlab/api.py
@@ -1,10 +1,11 @@
 import gitlab
 from packaging import version
 from os import getenv
+from typing import Union
 
 
-def get_gitlab_imgs_tags(base_url:str, token:str, project_id:str, container:str) -> dict:
-    """ For a specific project, extract all the images of the container registry, and return the tags sorted by the following logic:
+def get_gitlab_imgs_tags(base_url:str, token:str, project_id:str, image:str) -> Union[dict, None]:
+    """ For a specific project, extract all the images of the image registry, and return the tags sorted by the following logic:
     - The tag 'latest' has the bigger priority. If it exists, it will appear the first one.
     - The versions strings are sorted lexicographically.
     Finally, each array of tags will be sorted in descending order, thus placing the newest one in the first position.
@@ -16,18 +17,22 @@ def get_gitlab_imgs_tags(base_url:str, token:str, project_id:str, container:str)
                      It can be generated as explained here -> https://docs.gitlab.com/ee/user/profile/personal_access_tokens.html
         project_id (str): ID of the project accessible by the authenticated user.
                           It can be found at the repository main page in Gitlab.
-        container (str): Name of the container to extract tags for.
+        image (str): Name of the image to extract tags for.
+
+    Raises:
+        ValueError: The specified image, does not exist in the repository.
 
     Returns:
-        dict: Sorted tags, in the format {container_name : [latest_tag, previous_to_latest_tag, ...]}
+        dict: Sorted tags, in the format {image_name : [latest_tag, previous_to_latest_tag, ...]}
+        None: No image is found, the exception is raised
     """    
     gl = gitlab.Gitlab(base_url, private_token=token)
     project = gl.projects.get(id=project_id)
     plist = project.repositories.list(all=True)
     for p in plist:
-        if p.name == container:
+        if p.name == image:
             return [tag.name for tag in p.tags.list()]
-    raise ValueError(f'No container named {container} is present at the repository')
+    raise ValueError(f'No image named {image} is present at the repository')
 
 
 def get_latest_tag_for_image(image:str) -> str:
